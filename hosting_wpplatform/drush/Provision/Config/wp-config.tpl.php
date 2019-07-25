13e03d5159694b6054843c865c44c6bb36c85ae8
Mathieu Lutfy <mathieu@symbiotic.coop>
2019-07-25 20:13:09 +0200

    Add support for site-specific wp-config.php

--- hosting_wpplatform/drush/Provision/Config/wp-config.tpl.php
+++ hosting_wpplatform/drush/Provision/Config/wp-config.tpl.php
@@ -60,14 +60,20 @@ define('CIVICRM_CMSDIR', '<?php print $this->root; ?>');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+if (file_exists($_SERVER['wp_content_dir'] . '/../wp-config.php')) {
+  require_once $_SERVER['wp_content_dir'] . '/../wp-config.php';
+}
+
+if (!defined('AUTH_KEY')) {
+  define('AUTH_KEY',         'put your unique phrase here');
+  define('SECURE_AUTH_KEY',  'put your unique phrase here');
+  define('LOGGED_IN_KEY',    'put your unique phrase here');
+  define('NONCE_KEY',        'put your unique phrase here');
+  define('AUTH_SALT',        'put your unique phrase here');
+  define('SECURE_AUTH_SALT', 'put your unique phrase here');
+  define('LOGGED_IN_SALT',   'put your unique phrase here');
+  define('NONCE_SALT',       'put your unique phrase here');
+}
 
 /**#@-*/
 
