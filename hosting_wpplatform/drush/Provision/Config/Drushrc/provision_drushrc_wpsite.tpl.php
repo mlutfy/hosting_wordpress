7d8d2dbfbfa6bd1ce7562cdd968ab8dbdb8344f1
Mathieu Lutfy <mathieu@symbiotic.coop>
2022-05-19 15:38:10 -0400

    WP salts: define the salts, not

--- hosting_wpplatform/drush/Provision/Config/Drushrc/provision_drushrc_wpsite.tpl.php
+++ hosting_wpplatform/drush/Provision/Config/Drushrc/provision_drushrc_wpsite.tpl.php
@@ -16,7 +16,7 @@ $_SERVER['<?php print $key; ?>'] = $options['<?php print $key; ?>'];
 <?php } ?>
 # WordPress salts
 <?php foreach (['AUTH_KEY', 'SECURE_AUTH_KEY', 'LOGGED_IN_KEY', 'NONCE_KEY', 'AUTH_SALT', 'SECURE_AUTH_SALT', 'LOGGED_IN_SALT', 'NONCE_SALT'] as $key) { ?>
-$_SERVER['<?php print 'salt_' . $key; ?>'] = $options['salt_<?php print $key; ?>'];
+define('<?php print $key; ?>', $options['salt_<?php print $key; ?>']);
 <?php } ?>
 # local non-aegir-generated additions
 @include_once('<?php print $this->site_path  ?>/local.drushrc.php');
